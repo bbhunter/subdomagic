2bba700f4246ced70874344d803f0630a9b5fbcb
Angelo Alviar <angelo.alviar@90-m38dfd57-330.fireeye.com>
2019-06-24 14:14:36 -0700

    updating setup

--- subdomagic.sh
+++ subdomagic.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-GREEN = '\033[1;32'
-YELLOW = '\033[1;33'
+if [ ! -d "/opt/EyeWitness" ] || [-d "/opt/massdns"] || [-d "/opt/subfinder]; then
+  echo "Please run setup.sh"
+fi 
 
 # clear terminal (aesthetic!)
 clear
@@ -24,7 +25,7 @@ echo ""
 
 read nmapChoice
 
-echo -e "${YELLOW}[+] Making directory structure..."
+echo -e "[+] Making directory structure..."
 
 # make directory structure
 cd ./output
@@ -35,7 +36,7 @@ fi
 
 cd $domainName
 
-echo -e "${YELLOW}[+] Running subodmain enumeration...this may take a while..."
+echo -e "[+] Running subodmain enumeration...this may take a while..."
 
 # run amass
 amass enum -src -d $domainName -o $domainName-amass.txt
@@ -46,14 +47,14 @@ amass enum -src -d $domainName -o $domainName-amass.txt
 # run massdns
 ./scripts/subbrute.py lists/names.txt $domainName | ./bin/massdns -r lists/resolvers.txt -t A -o S -w $domainName-massdns.txt
 
-echo -e "${YELLOW}[+] Consolidating subdomain findings..."
+echo -e "[+] Consolidating subdomain findings..."
 
 # dedup all subdomain findings 
 cat $domainName-amass.txt $domainName-subfinder.txt $domainName-massdns.txt > $domainName-subdomains.txt
 
 sort $domainName-subdomains.txt | uniq -u > $domainName-subdomains.txt
 
-echo -e "${YELLOW}[+] Conducting initial scan..."
+echo -e "[+] Conducting initial scan..."
 
 # run nmap scan for host discovery/web/few common ports
 nmap -oA $domainName-nmap-fast --stats-every 60s --log-errors --traceroute --reason --randomize-hosts -v -R -PE -PM -PO -PU -PS80,23,443,21,22,25,3389,110,445,139 -PA80,443,22,445,139 -sS -sV -p21,22,23,25,80,443,8080,8443 $domainName-subdomains.txt
@@ -68,7 +69,7 @@ fi
 # if statement for choice 2, nmap tcp top 1000
 if [ $nmapChoice = "2" ]             
 then
-    echo -e "${YELLOW}[+] Conducting comprehensive scan..."
+    echo -e "[+] Conducting comprehensive scan..."
     #grep for online hosts
     grep -i "status: up" $domainName-nmap-fast.gnmap | awk -F" " '{print $2}' > $domainName-online.txt
 
@@ -79,7 +80,7 @@ then
     cat $domainName-comprehensive.gnmap | grep "open[^, ]*\(http\|sip\|ipp\|oem-agent\|soap\|snet-sensor-mgmt\|connect-proxy\|cpq-wbem\|event-port\|analogx\|proxy-plus\|saphostctrl\|saphostctrls\|spss\|sun-answerbook\|wsman\|wsmans\|wso2esb-console\|xmpp-bosh\)" | cut -d" " -f 2 | sort -u > $domainName-webservers.txt
 fi 
 
-echo -e "${YELLOW}[+] Screenshotting webservers with EyeWitness..."
+echo -e "[+] Screenshotting webservers with EyeWitness..."
 # Run EyeWitness
 ./EyeWitness.py -f $domainName-webservers.txt --web
 
