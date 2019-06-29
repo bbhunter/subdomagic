98f913d702364b35330fdc3582b9264945b4595c
frag <fragtastic@users.noreply.github.com>
2019-06-29 15:53:11 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO - check for dependency installations
 if [ ! d "output" ]; then
   exit "You have already installed subdomagic!"
 fi 
@@ -7,18 +8,7 @@ fi
 echo -e "\e[102m[+] Installing dependencies....\e[49m"
 
 mkdir output
-
-#update
-sudo apt-get update
-
-# install snap 
-sudo apt install snapd
-sudo systemctl start snapd
-sudo systemctl enable snapd
-sudo systemctl start apparmor
-sudo systemctl enable apparmor
-export PATH=$PATH:/snap/bin
-sudo snap refresh
+_last=`pwd`
 
 #install go
 sudo snap install --classic go
@@ -26,37 +16,38 @@ sudo snap install --classic go
 echo -e "\e[102m[+] Installing amass...\e[49m"
 
 #install amass
-sudo snap install amass
+go get -u github.com/OWASP/Amass/...
+cd $GOPATH/src/github.com/OWASP/Amass
+go install ./...
+cd _last
 
 echo -e "\e[102m[+] Installing Subfinder...\e[49m"
 #install subfinder
-cd /opt
-git clone https://github.com/subfinder/subfinder
+git clone https://github.com/subfinder/subfinder 
 cd subfinder
 go get github.com/subfinder/subfinder
 sh build.sh
 go build
+cd _last
 
 echo -e "\e[102m[+] Installing massdns...\e[49m"
 #install massdns
-cd /opt
 git clone https://github.com/blechschmidt/massdns
 cd massdns
 make
 
-cd /opt/massdns/lists
+cd lists
 wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
+cd _last
 
 echo -e "\e[102m[+] Installing Eyewitness...\e[49m"
 
 #install EyeWitness
-cd /opt
 git clone https://github.com/FortyNorthSecurity/EyeWitness
 cd EyeWitness
 cd setup
 ./setup.sh
-
-clear
+cd _last
 
 #complete setup
 echo -e "\e[1msubdomagic by gelosecurity.com\e[22m"
